373d32de8ec14712f7fada982eeb852baa37910e
quickscraper <app@quickscraper.co>
2024-06-27 16:30:25 +0530

    removed space

--- timeout_feature_sample.py
+++ timeout_feature_sample.py
@@ -1,4 +1,3 @@
-
 from quickscraper_sdk import QuickScraper
 quickscraper_client = QuickScraper('ACCESS_TOKEN')
 response = quickscraper_client.getHtml('https://www.google.com/', isScroll=True, scrollTimeout=30000)
