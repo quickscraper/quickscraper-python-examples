e21b4dd6e165bf5f14cc0673a1f965b4505a1a44
quickscraper <app@quickscraper.co>
2024-06-27 19:41:01 +0530

    Added real example

--- actions_feature_sample.py
+++ actions_feature_sample.py
@@ -1,10 +1,19 @@
 
 from quickscraper_sdk import QuickScraper
 quickscraper_client = QuickScraper('ACCESS_TOKEN')
-response = quickscraper_client.getHtml('https://www.google.com/',keepSelection=True,customSelectors=[{
-				'isScript': 'BOOLEAN',
-				'options':'click',
-				'selectorScript':'SELECT_INPUT',
+response = quickscraper_client.getHtml('https://www.prismetric.com/job-opportunities/',keepSelection=True,customSelectors=[{
+				'name':'click',
+    			'isScript': 'False',
+				'options':'',
+				'selectorScript':'a[href="https://www.prismetric.com/taxi-booking-app-development-solutions/"]',
+				}]
+			)
+print(response.content)
+response = quickscraper_client.getHtml('https://www.prismetric.com/',keepSelection=True,customSelectors=[{
+				'name':'waitForElement',
+    			'isScript': 'False',
+				'options':'',
+				'selectorScript':'a[href="https://www.prismetric.com/mobile-app-development/"]',
 				}]
 			)
 print(response.content)
