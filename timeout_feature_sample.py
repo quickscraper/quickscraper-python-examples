cba8a2b1b822883a252fe3460ef19de1a62a2ea1
quickscraper <app@quickscraper.co>
2024-06-27 15:51:12 +0530

    Added sample timeout example

---
+++ timeout_feature_sample.py
@@ -0,0 +1,5 @@
+
+from quickscraper_sdk import QuickScraper
+quickscraper_client = QuickScraper('ACCESS_TOKEN')
+response = quickscraper_client.getHtml('https://www.google.com/', isScroll=True, scrollTimeout=30000)
+print(response.content)
